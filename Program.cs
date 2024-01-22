af49ff1c703acc80963e2718051731035512e7d2
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-01-22 15:32:13 -0400

    Implementado Iphone e Testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("\n");
+Console.WriteLine("Nokia rodando");
+Nokia nokia = new Nokia("67981894046", "3320", "352391980375742", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Minecraft");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone Rodando");
+Iphone iphone = new Iphone("67992158616", "Iphone XR", "356423109411037", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("LastPass");
